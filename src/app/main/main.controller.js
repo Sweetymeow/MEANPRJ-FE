830be199e5ece268a315ba1e10bb0fc1dc1f0141
i849308 <xin.wen@sap.com>
2017-04-09 19:42:31 -0700

    Finished Chapter3: Display Mesages(show MongoDB data in Page)

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,8 +3,17 @@ export class MainController {
         'ngInject';
 
         this.$http = $http;
+        this.getMessages();
     }
 
+    getMessages() {
+        var vm = this; // view model
+        this.$http.get('http://localhost:5000/api/message')
+            .then(function(result){
+                vm.theMessages = result.data;    
+            //console.log(result);
+            });
+    }
     postMessage() {
         this.$http.post('http://localhost:5000/api/message', {
             msg: this.message
